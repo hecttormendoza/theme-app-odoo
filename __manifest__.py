d9f54290321cc615d909738fb8d02587a998ad40
Héctor Mendoza <hecttormendoza@gmail.com>
2018-08-21 16:56:57 -0500

    [WIP]: Theme tutorial app / mass_mailing

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,21 @@
 {
     'name': 'Theme Tutorial',
     'category': 'Theme/Creative',
+    'author': 'Héctor Mendoza',
     'description': 'Theme Tutorial App',
     'version': '11.0',
-    'depends': ['website',],
-    'data': [],
+    'depends': [
+        'website',
+        'mass_mailing',
+    ],
+    'data': [
+        'views/assets.xml',
+        'views/image_gallery.xml',
+        'views/snippets.xml',
+        'views/mail_snippets.xml',
+        'views/options.xml',
+        'views/layout.xml',
+        'views/pages.xml',
+    ],
     'demo': [],
 }
