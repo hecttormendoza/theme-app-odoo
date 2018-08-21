f67d6e4115393bdf48c374b2dbe3c512b2538980
Héctor Mendoza <hecttormendoza@gmail.com>
2018-08-21 09:56:43 -0500

    [WIP] Added init files

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Theme Tutorial',
+    'category': 'Theme/Creative',
+    'description': 'Theme Tutorial App',
+    'version': '11.0',
+    'depends': ['website',],
+    'data': [],
+    'demo': [],
+}
